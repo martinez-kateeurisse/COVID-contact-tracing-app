dcd0d0124da4373b423b8678ff3513f7cbe884a7
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-15 15:42:04 +0800

    File handling: getting inputs

--- file_handling.py
+++ file_handling.py
@@ -1,5 +1,11 @@
 #Codes for file handling will be on this python file
 
 #Create class
+class FileHandling:
 #Create object of class
+    def save_inputs(self):
+        self.name = self.input_name.get()
+        self.age = self.input_age.get()
+        self.address = self.input_address.get()
+        self.phone_number = self.input_number.get()
     #Write or store the data gathered from the user in a text file
