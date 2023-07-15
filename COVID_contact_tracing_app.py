30dc9916f1104ae45702777e92a056f72fe265e0
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-15 11:58:13 +0800

    Main file created w/ general program instruction

---
+++ COVID_contact_tracing_app.py
@@ -0,0 +1,7 @@
+#Kate Eurisse L. Martinez_BSCPE 1-5_COVID Contact Tracing App
+
+#Create “your” own covid contact tracing app with GUI
+#Create a program that ask user for typical information found in covid contact tracing app
+#Write the collected information in a file (use any format you like)
+#The app should be able to add and search entry
+#Be creative, the realistic the better.
