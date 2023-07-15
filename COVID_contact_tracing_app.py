6efdb6e6b51570287753efaa7982eaed4b85fa9b
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-15 14:47:19 +0800

    labels added(name, age, address, number)

--- COVID_contact_tracing_app.py
+++ COVID_contact_tracing_app.py
@@ -7,7 +7,7 @@
 #The app should be able to add and search entry
 #Be creative, the realistic the better.
 
-#Pseudocode
+
 
 #Import tkinter (GUI)
 import tkinter as tk
@@ -17,10 +17,18 @@ main = tk.Tk() #Creating the application main window
 main.title("COVID Contact Tracing APP") #Main window title
 main.geometry("900x700") #Main window Size
 
-#Add Widgets 
-	#Buttons
-	#Labels
-	#Ask user for certain information (Possible ones to be used)
+#Add Widgets
+#Ask user for certain information (Possible ones to be used)
+#Labels
+label_name = tk.Label(main, text ="Name: ") #For the user's name
+label_name.grid(row = 1, column = 0, padx = 5, pady = 5)
+label_age = tk.Label(main, text ="Age: ") #For the user's age
+label_age.grid(row = 2, column = 0, padx = 5, pady = 5)
+label_address = tk.Label(main, text ="Address: ") #For the user's address
+label_address.grid(row = 3, column = 0, padx = 5, pady = 5)
+label_number = tk.Label(main, text ="Phone number: ") #For the user's phone number
+label_number.grid(row = 4, column = 0, padx = 5, pady = 5)
+		#Buttons
 		#Entry
 		#Text
 		#Radiobutton
