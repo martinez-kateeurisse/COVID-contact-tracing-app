f373b2639b5a802804f764433695babb81ba5513
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-15 16:21:45 +0800

    Main: calling data methods

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from tk_methods import TkMethods
 #Initialize class
 gui = TkMethods()
 
-#Calling the method for the name data
+#Calling the method for the name's data
 gui.name()
-
+#Calling the method for the age's data
+gui.age()
+#Calling the method for the address' data
+gui.address()
+#Calling the method for the number's data
+gui.number()
 #Running the mainloop
 gui.main.mainloop()
