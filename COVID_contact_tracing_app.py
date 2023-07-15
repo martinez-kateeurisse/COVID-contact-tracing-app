5f27ba3388685201148170e097e12a87b250b009
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-15 12:25:22 +0800

    Pseudocode (program flow draft)

--- COVID_contact_tracing_app.py
+++ COVID_contact_tracing_app.py
@@ -1,7 +1,24 @@
 #Kate Eurisse L. Martinez_BSCPE 1-5_COVID Contact Tracing App
 
+# Program General Instruction
 #Create “your” own covid contact tracing app with GUI
 #Create a program that ask user for typical information found in covid contact tracing app
 #Write the collected information in a file (use any format you like)
 #The app should be able to add and search entry
 #Be creative, the realistic the better.
+
+#Pseudocode
+
+#Import tkinter (GUI)
+#Create Window
+#Add Widgets 
+	#Buttons
+	#Labels
+	#Ask user for certain information (Possible ones to be used)
+		#Entry
+		#Text
+		#Radiobutton
+#Write or store the data gathered from the user in a text file
+	#Let user search for info and display it
+
+#Call the main event loop
