55cce3038fb97cb2923fbd6a558bc68fc07b8dbc
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-16 22:56:13 +0800

    Trying to execute something, but having a hard time. so, will continue later or tomorrow.(will eat first hshs)

--- tk_methods.py
+++ tk_methods.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from file_handling import FileHandling
+from tkinter import ttk
+
 class TkMethods(FileHandling):
     #Defining main window
     def main_window (self):
@@ -8,14 +10,15 @@ class TkMethods(FileHandling):
         self.main.geometry("600x900") #Main window Size
         window_title = tk.Label(self.main, text="COVID Contact Tracing APP", font=("Courier 10 pitch", 14), bg=self.main['bg']) #Window Title
         window_title.grid(row=0, column=0, columnspan=3, sticky='ew', padx=10)
-                  
+
     #Defining Buttons
     def submit_button (self):
         button_submit = tk.Button(self.main, text="Submit", command = self.save_inputs)
         button_submit.grid(row=30, column=1, padx=10, pady=10)
+    #Saving inputs
     def save_inputs(self):
         txt = FileHandling()
         user_symptoms = [option.get() for option in self.check_symptom]
         txt.save_inputs(self.input_name.get(), self.input_age.get(), self.input_address.get(), self.input_number.get(), self.var.get(), user_symptoms, self.ex_var.get(),
                         self.contact_var.get())
-        txt.storage_file()
+        txt.storage_file()    
