0d2e89d5278362b499c32de608a36c0b232d3b85
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-17 14:13:02 +0800

    Variable names-modified(window changes)

--- tk_methods.py
+++ tk_methods.py
@@ -3,7 +3,7 @@ from file_handling import FileHandling
 from tkinter import ttk
 
 class TkMethods(FileHandling):
-    #Defining main window
+    #Defining first window
     def first_window (self):
         self.main = tk.Tk() #Creating the application main window
         self.main.title("COVID Contact Tracing APP") #Main window title
@@ -11,24 +11,28 @@ class TkMethods(FileHandling):
         self.main.columnconfigure(0, weight=1)
         window_title = tk.Label(self.main, text="COVID Contact Tracing APP", font=("Courier 10 pitch", 14), bg=self.main['bg']) #Window Title
         window_title.grid(row=0, column=0, columnspan=5, sticky='ew', padx=10) 
+    
+    #Defining the second window
     def second_window (self):
         self.second = tk.Tk()
         self.second.title("COVID Contact Tracing APP - Personal Information")
-        self.main.geometry("700x700")
-        self.main.columnconfigure(0, weight=1)
-        window_title = tk.Label(self.main, text="COVID Contact Tracing APP", font=("Courier 10 pitch", 14), bg=self.main['bg']) 
+        self.second.geometry("700x700")
+        self.second.columnconfigure(0, weight=1)
+        window_title = tk.Label(self.second, text="COVID Contact Tracing APP", font=("Courier 10 pitch", 14), bg=self.second['bg']) 
         window_title.grid(row=0, column=0, columnspan=5, sticky='ew', padx=10)
+    
+    #Defining the third window
     def third_window (self):
-        self.second = tk.Tk()
-        self.second.title("COVID Contact Tracing APP - Health Declaration")
-        self.main.geometry("700x700")
-        self.main.columnconfigure(0, weight=1)
-        window_title = tk.Label(self.main, text="COVID Contact Tracing APP", font=("Courier 10 pitch", 14), bg=self.main['bg']) 
-        window_title.grid(row=0, column=0, columnspan=5, sticky='ew', padx=10)
-        
+        self.third = tk.Tk()
+        self.third.title("COVID Contact Tracing APP - Health Declaration")
+        self.third.geometry("700x700")
+        self.third.columnconfigure(0, weight=1)
+        window_title = tk.Label(self.third, text="COVID Contact Tracing APP", font=("Courier 10 pitch", 14), bg=self.third['bg']) 
+        window_title.grid(row=0, column=0, columnspan=5, sticky='ew', padx=10) 
+
     #Defining Buttons
     def submit_button (self):
-        button_submit = tk.Button(self.main, text="Submit", command = self.save_inputs)
+        button_submit = tk.Button(self.third, text="Submit", command = self.save_inputs)
         button_submit.grid(row=30, column=1, padx=10, pady=10)
 
     #Saving inputs
--- user_interface.py
+++ user_interface.py
@@ -4,31 +4,31 @@ from tk_methods import TkMethods
 class UserInterface(TkMethods):
 #Personal Info section
     def personal_info (self):
-        self.section1_title = tk.Label(self.main, text="Personal Information", font=("Courier 10 pitch", 12), bg=self.main['bg']) #Section Title
+        self.section1_title = tk.Label(self.second, text="Personal Information", font=("Courier 10 pitch", 12), bg=self.second['bg']) #Section Title
         self.section1_title.grid(row=1, column=0, columnspan=3, sticky='ew', padx=10)     
         #Defining method for Name's data
         def name (self):
-            label_name = tk.Label(self.main, text ="Name: ") #For the user's name
+            label_name = tk.Label(self.second, text ="Name: ") #For the user's name
             label_name.grid(row = 2, column = 0, padx = 5, pady = 5)
-            self.input_name = tk.Entry(self.main)		#For the user's name
+            self.input_name = tk.Entry(self.second)		#For the user's name
             self.input_name.grid(row = 2, column = 1, padx = 5, pady = 5)
         #Defining method for Age's data
         def age (self):
-            label_age = tk.Label(self.main, text ="Age: ") #For the user's age
+            label_age = tk.Label(self.second, text ="Age: ") #For the user's age
             label_age.grid(row = 3, column = 0, padx = 5, pady = 5)
-            self.input_age = tk.Entry(self.main)		#For the user's age
+            self.input_age = tk.Entry(self.second)		#For the user's age
             self.input_age.grid(row = 3, column = 1, padx = 5, pady = 5)	
         #Defining method for Address' data
         def address (self):
-            label_address = tk.Label(self.main, text ="Address: ") #For the user's address
+            label_address = tk.Label(self.second, text ="Address: ") #For the user's address
             label_address.grid(row = 4, column = 0, padx = 5, pady = 5)
-            self.input_address = tk.Entry(self.main)		#For the user's address
+            self.input_address = tk.Entry(self.second)		#For the user's address
             self.input_address.grid(row = 4, column = 1, padx = 5, pady = 5)	
         #Defining method for Number's data
         def number (self):
-            label_number = tk.Label(self.main, text ="Phone number: ") #For the user's phone number
+            label_number = tk.Label(self.second, text ="Phone number: ") #For the user's phone number
             label_number.grid(row = 5, column = 0, padx = 5, pady = 5)
-            self.input_number = tk.Entry(self.main)		#For the user's number
+            self.input_number = tk.Entry(self.second)		#For the user's number
             self.input_number.grid(row = 5, column = 1, padx = 5, pady = 5)	
         name(self)
         age(self)
@@ -37,8 +37,8 @@ class UserInterface(TkMethods):
 
     #Health Declaration section
     def health_info (self):
-        self.section1_title = tk.Label(self.main, text="Health Declaration", font=("Courier 10 pitch", 12), bg=self.main['bg']) #Section Title
-        self.section1_title.grid(row=7, column=0, columnspan=3, sticky='ew', padx=10)
+        self.section2_title = tk.Label(self.third, text="Health Declaration", font=("Courier 10 pitch", 12), bg=self.third['bg']) #Section Title
+        self.section2_title.grid(row=7, column=0, columnspan=3, sticky='ew', padx=10)
         self.vaccination_status() #Calling vaccination status method
         self.get_symptoms() #Calling symptoms data method
         self.exposure() #Calling exposure data method
@@ -46,18 +46,18 @@ class UserInterface(TkMethods):
         self.test()  #Calling COVID test data method
     #First Question (Vaccination status)
     def vaccination_status (self):
-        label_vaccine = tk.Label(self.main, text ="Have you been vaccinated for COVID-19?") #Question label
+        label_vaccine = tk.Label(self.third, text ="Have you been vaccinated for COVID-19?") #Question label
         label_vaccine.grid(row = 8, column = 0, padx = 5, pady = 5, sticky = "w") #Position
         self.var = tk.StringVar()    #Initializing Variable    
         #Initializing options in a list
         vac_status = ["Not Yet", "1st Dose", "2nd Dode (Fully Vaccinated)", "1st Booster Shot", "2nd Booster Shot"]
         #Setting radiobuttons for the question
         for i in range (len(vac_status)):
-            self.input_vaccine = tk.Radiobutton(self.main, text=vac_status[i], variable = self.var, value = vac_status[i]) 
+            self.input_vaccine = tk.Radiobutton(self.third, text=vac_status[i], variable = self.var, value = vac_status[i]) 
             self.input_vaccine.grid(row = 9+i, column = 0, padx = 0, pady = 0, sticky = "w")	     
     #Second Question (Symptoms)
     def get_symptoms(self):
-        label_vaccine = tk.Label(self.main, text="Are you experiencing any symptoms in the past 7 days such as:")
+        label_vaccine = tk.Label(self.third, text="Are you experiencing any symptoms in the past 7 days such as:")
         label_vaccine.grid(row=14, column=0, padx=5, pady=5, sticky="w")
         
         #Initializing Symptoms option
@@ -67,23 +67,23 @@ class UserInterface(TkMethods):
         #Checking symptoms data 
         for i, option in enumerate(options):
             var = tk.StringVar()
-            input_symptom = tk.Checkbutton(self.main, text=option, variable=var, onvalue = option, offvalue="", anchor = "w")
+            input_symptom = tk.Checkbutton(self.third, text=option, variable=var, onvalue = option, offvalue="", anchor = "w")
             input_symptom.grid(row=15 + (i // 2), column=i % 2, padx=0, pady=0, sticky="w")
             self.check_symptom.append(var)
     #Third Question (Exposure to a probable positive case)
     def exposure(self):
-        label_exposure = tk.Label(self.main, text ="Have you had exposure to a probable or confirmed case in the last 14 days?")#Question label
+        label_exposure = tk.Label(self.third, text ="Have you had exposure to a probable or confirmed case in the last 14 days?")#Question label
         label_exposure.grid(row=21, column=0, padx=5, pady= 5, sticky="w") #Position
         self.ex_var = tk.StringVar()    #Initializing Variable    
         #Initializing options in a list
         ex_status = ["Yes", "No", "Uncertain"]
         #Setting radiobuttons for the question
         for i in range (len(ex_status)):
-            self.input_exposure = tk.Radiobutton(self.main, text=ex_status[i], variable = self.ex_var, value = ex_status[i]) 
+            self.input_exposure = tk.Radiobutton(self.third, text=ex_status[i], variable = self.ex_var, value = ex_status[i]) 
             self.input_exposure.grid(row = 22+i, column = 0, padx = 0, pady = 0, sticky = "w")
     #Fourth Question (Contact with someone with symptoms)
     def contact (self):
-        label_contact = tk.Label(self.main, text="Have you had in contact with somebody with body pains\n"
+        label_contact = tk.Label(self.third, text="Have you had in contact with somebody with body pains\n"
                                          "headache, sore throat, fever, diarrhea, caugh, colds, shortness of breath,\n"
                                          "loss of taste or loss of smell in the past 7 days?",
                          justify="left")  #Question label
@@ -93,16 +93,16 @@ class UserInterface(TkMethods):
         contact_status = ["Yes", "No"]
         #Setting radiobuttons for the question
         for i in range (len(contact_status)):
-            self.input_contact = tk.Radiobutton(self.main, text=contact_status[i], variable = self.contact_var, value = contact_status[i])
+            self.input_contact = tk.Radiobutton(self.third, text=contact_status[i], variable = self.contact_var, value = contact_status[i])
             self.input_contact.grid(row=25+i, column = 0, padx = 0, pady = 0, sticky ="w")
     #Fifth Question (If tested for COVID)
     def test (self):
-        label_test = tk.Label(self.main, text="Have you been tested for COVID-19 in the last 14 days?") #Question label
+        label_test = tk.Label(self.third, text="Have you been tested for COVID-19 in the last 14 days?") #Question label
         label_test.grid(row = 28, column =0, padx = 5, pady =5, sticky ="w") #Position
         self.test_var = tk.StringVar() #Initializing Variable
         #Initializing options in a list
         test_status = ["No", "Yes - Positive", "Yes - Negative", "Yes - Pending"]
         #Setting radiobuttons for the question
         for i in range (len(test_status)):
-            self.input_test = tk.Radiobutton(self.main, text = test_status[i], variable = self.test_var, value = test_status[i]) 
+            self.input_test = tk.Radiobutton(self.third, text = test_status[i], variable = self.test_var, value = test_status[i]) 
             self.input_test.grid(row=29+i, column = 0, padx=0, pady=0, sticky ="w")
