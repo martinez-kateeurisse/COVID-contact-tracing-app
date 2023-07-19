ae279b3ca5720a81638d536e2e07697040434625
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-19 20:06:32 +0800

    Form: Window, canvas, scrollbar - created (methods are not pushed one by one since codes are just reused and modified from my first program try)

--- form_tk.py
+++ form_tk.py
@@ -2,7 +2,43 @@
 
 #This python file will serve as the main file for the form (second frame/window)
 
-#Import necessary modules
-#Create Class
-#Add methods
-#Call methods
+#Import Necessary modules
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+#Import class
+from form_user_interface import UserInterface
+
+#Create class
+class FormTk(UserInterface):
+    def __init__(self):
+        main = tk.Tk()
+        main.title("COVID Contact Tracing APP") #Title
+        main.geometry("820x700")  # Window Size
+
+        # Creating a Canvas
+        canvas = tk.Canvas(main)
+        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Adding a Scrollbar
+        scrollbar = ttk.Scrollbar(main, command=canvas.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # Configuring the Canvas
+        canvas.configure(yscrollcommand=scrollbar.set)
+        canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+        # Creating a Frame inside the Canvas
+        self.frame = tk.Frame(canvas)
+
+        # Adding the Frame to the Canvas
+        canvas.create_window((0, 0), window=self.frame, anchor="nw")
+        
+        # Updating the Canvas scrollregion
+        canvas.update_idletasks()
+
+        #Run mainloop
+        main.mainloop()
+
+
