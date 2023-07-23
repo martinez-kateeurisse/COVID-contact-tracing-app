84b540644f04d9521dc681dfabde9c007e407a2f
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-23 15:39:25 +0800

    Intro, intro ui: form open button added and called

--- intro.py
+++ intro.py
@@ -6,25 +6,29 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-
+from intro_ui import IntroUserInterface
 #Create class
-class IntroWindow:
+class IntroWindow(IntroUserInterface):
 #Create window
     def __init__(self):
-        intro = tk.Tk()
-        intro.title("COVID Contact Tracing APP") #Title
-        intro.geometry("1020x700") #Window size
+        self.intro = tk.Tk()
+        self.intro.title("COVID Contact Tracing APP") #Title
+        self.intro.geometry("1020x700") #Window size
     #Create Canvas
-        intro_canvas = tk.Canvas(intro)
+        intro_canvas = tk.Canvas(self.intro)
         intro_canvas.grid(columnspan=3)
     #Add Image
         intro_logo = Image.open("Intro_logo.png")
         intro_logo = ImageTk.PhotoImage(intro_logo)
         logo_label = tk.Label(image = intro_logo)
         logo_label.image = intro_logo
-        logo_label.grid(column=2, row=0)
+        logo_label.grid(column=0, row=0)
+    #Call open button
+        self.open_form()
     #Run mainloop
-        intro.mainloop()
+        self.intro.mainloop()
 
 #Call the class (test)
 intro_gui = IntroWindow()
+
+
--- intro_ui.py
+++ intro_ui.py
@@ -2,8 +2,17 @@
 
 #This python file will have the codes for introduction user interface
 
+#Import modules
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
 #Create Class
-#Create instance variables
+class IntroUserInterface:
     #Button for entering form window
+    def open_form(self):
+        button_open_form = tk.Button(self.intro, text ="Start Contact Tracing Form", bg = "plum1", fg="DarkOrchid4")
+        button_open_form.grid(row = 3, column = 0, sticky ="news", padx=20, pady=20)
+
     #Search Engine
     #Displaying search result
