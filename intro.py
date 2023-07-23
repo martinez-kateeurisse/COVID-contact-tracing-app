2ed19c975af5e6f515918f97b865f2dc422f3bfb
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-23 14:20:21 +0800

    I'mmmm backkkkk (been so busy:<<) ...adding intro window

--- intro.py
+++ intro.py
@@ -1,3 +1,12 @@
 #Kate Eurisse L. Martinez_BSCPE 1-5_COVID Contact Tracing App
 
 #This python file will have the codes for introduction window
+
+#Import necessary modules
+
+#Create class
+#Create window
+#Create Canvas
+#Add Image
+
+#Run mainloop
