c8fc17ce60891d040e569257d0746e7dac1ab1bc
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-23 14:27:31 +0800

    Intro: window created(+ mainloop)

--- intro.py
+++ intro.py
@@ -10,7 +10,19 @@ from PIL import Image, ImageTk
 #Create class
 class IntroWindow:
 #Create window
-#Create Canvas
-#Add Image
+    def __init__(self):
+        intro = tk.Tk()
+        intro.title("COVID Contact Tracing APP") #Title
+        intro.geometry("1020x700") #Window size
 
-#Run mainloop
+    
+
+
+    #Create Canvas
+    #Add Image
+
+    #Run mainloop
+        intro.mainloop()
+        
+#Call the class (test)
+intro_gui = IntroWindow()
