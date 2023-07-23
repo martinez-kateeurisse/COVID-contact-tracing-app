b4ef401f0fb7d95aea98766e662ee54e23c2d2ff
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-24 03:07:40 +0800

    file handling: csv file defined(try)

--- file_handling.py
+++ file_handling.py
@@ -1,6 +1,13 @@
 #This python file will serve as the data handling file
 
-#Inmport modules
+#Import modules
+import csv
+
 #Create class
-#Open csv file
-#Append data
+class FileHandling:
+    #Open csv file and append data
+    def add_data_to_csv(file_path, data):
+        with open(file_path, mode='a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data)
+
