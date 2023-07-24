6c6a5b86f4d399fa2271cfefc4801d22ebb10107
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-24 12:58:57 +0800

    form ui, modified - added self. to define - csv working (personal info saved)... coding while waiting for them

--- form_user_interface.py
+++ form_user_interface.py
@@ -24,98 +24,98 @@ class UserInterface:
     #Defining method for Name's data
     def name (self):
         #Name labels
-        label_last_name = tk.Label(self.personal_info_frame, text="Last Name")
-        label_last_name.grid(row=0, column=0)
-        label_first_name = tk.Label(self.personal_info_frame, text="First Name")
-        label_first_name.grid(row=0, column=1)
-        label_middle_name = tk.Label(self.personal_info_frame, text="Middle Name")
-        label_middle_name.grid(row=0, column=2)
+        self.label_last_name = tk.Label(self.personal_info_frame, text="Last Name")
+        self.label_last_name.grid(row=0, column=0)
+        self.label_first_name = tk.Label(self.personal_info_frame, text="First Name")
+        self.label_first_name.grid(row=0, column=1)
+        self.label_middle_name = tk.Label(self.personal_info_frame, text="Middle Name")
+        self.label_middle_name.grid(row=0, column=2)
 
         #Name entry fields
-        input_last_name = tk.Entry(self.personal_info_frame)
-        input_first_name = tk.Entry(self.personal_info_frame)
-        input_middle_name = tk.Entry(self.personal_info_frame)
-        input_last_name.grid(row=1, column=0, padx=5, pady=6) 
-        input_first_name.grid(row=1, column=1, padx=5, pady=6) 
-        input_middle_name.grid(row=1, column=2, padx=5, pady=6) 
+        self.input_last_name = tk.Entry(self.personal_info_frame)
+        self.input_first_name = tk.Entry(self.personal_info_frame)
+        self.input_middle_name = tk.Entry(self.personal_info_frame)
+        self.input_last_name.grid(row=1, column=0, padx=5, pady=6) 
+        self.input_first_name.grid(row=1, column=1, padx=5, pady=6) 
+        self.input_middle_name.grid(row=1, column=2, padx=5, pady=6) 
     
     #Defining method for Age's data
     def age (self):
         #Age label, inputs(spinbox)
-        label_age = tk.Label(self.personal_info_frame, text="Age")
-        age_spinbox = tk.Spinbox(self.personal_info_frame, from_=1, to=120, width = 10)
-        label_age.grid(row=0, column=3)
-        age_spinbox.grid(row=1, column=3, padx =10, pady=5)
+        self.label_age = tk.Label(self.personal_info_frame, text="Age")
+        self.age_spinbox = tk.Spinbox(self.personal_info_frame, from_=1, to=120, width = 10)
+        self.label_age.grid(row=0, column=3)
+        self.age_spinbox.grid(row=1, column=3, padx =10, pady=5)
     
     #Defining method for Gender's data
     def gender(self):
         #Gender labels, inputs(combobox)
-        label_gender = tk.Label(self.personal_info_frame, text="Gender")
-        gender_combobox = ttk.Combobox(self.personal_info_frame, values = ["Male", "Female", "Other"], width=7)
-        label_gender.grid(row=0, column = 4)
-        gender_combobox.grid(row=1, column = 4,padx =10, pady=5)
+        self.label_gender = tk.Label(self.personal_info_frame, text="Gender")
+        self.gender_combobox = ttk.Combobox(self.personal_info_frame, values = ["Male", "Female", "Other"], width=7)
+        self.label_gender.grid(row=0, column = 4)
+        self.gender_combobox.grid(row=1, column = 4,padx =10, pady=5)
     
     #Defining method for Address data
     def address(self):
         #Address labels
-        address_title=tk.Label(self.personal_info_frame, text="Current Address:", font=("Tahoma", 10, "bold"))
-        address_title.grid(row=2, column=0, sticky="w")
-        label_street_address = tk.Label(self.personal_info_frame, text="Street/Blck/House No.")
-        label_street_address.grid(row=3, column=0)
-        label_city_address = tk.Label(self.personal_info_frame, text="City/Town")
-        label_city_address.grid(row=3, column=1)
-        label_state_address = tk.Label(self.personal_info_frame, text="State/Province")
-        label_state_address.grid(row=3, column=2)
-        label_country_address = tk.Label(self.personal_info_frame, text="Country")
-        label_country_address.grid(row=3, column=3)
-        label_postal_code = tk.Label(self.personal_info_frame, text="Postal Code")
-        label_postal_code.grid(row=3, column=4)
+        self.address_title=tk.Label(self.personal_info_frame, text="Current Address:", font=("Tahoma", 10, "bold"))
+        self.address_title.grid(row=2, column=0, sticky="w")
+        self.label_street_address = tk.Label(self.personal_info_frame, text="Street/Blck/House No.")
+        self.label_street_address.grid(row=3, column=0)
+        self.label_city_address = tk.Label(self.personal_info_frame, text="City/Town")
+        self.label_city_address.grid(row=3, column=1)
+        self.label_state_address = tk.Label(self.personal_info_frame, text="State/Province")
+        self.label_state_address.grid(row=3, column=2)
+        self.label_country_address = tk.Label(self.personal_info_frame, text="Country")
+        self.label_country_address.grid(row=3, column=3)
+        self.label_postal_code = tk.Label(self.personal_info_frame, text="Postal Code")
+        self.label_postal_code.grid(row=3, column=4)
 
         #Address entry fields
-        input_street_address = tk.Entry(self.personal_info_frame)
-        input_city_address = tk.Entry(self.personal_info_frame)
-        input_state_address = tk.Entry(self.personal_info_frame)
-        input_country_address = tk.Entry(self.personal_info_frame)
-        input_postal_code = tk.Entry(self.personal_info_frame, width = 10)
-        input_street_address.grid(row=4, column=0, padx=5, pady=5) 
-        input_city_address.grid(row=4, column=1, padx=5, pady=5) 
-        input_state_address.grid(row=4, column=2, padx=5, pady=5) 
-        input_country_address.grid(row=4, column=3, padx=5, pady=5) 
-        input_postal_code.grid(row=4, column=4, padx=5, pady=5)
+        self.input_street_address = tk.Entry(self.personal_info_frame)
+        self.input_city_address = tk.Entry(self.personal_info_frame)
+        self.input_state_address = tk.Entry(self.personal_info_frame)
+        self.input_country_address = tk.Entry(self.personal_info_frame)
+        self.input_postal_code = tk.Entry(self.personal_info_frame, width = 10)
+        self.input_street_address.grid(row=4, column=0, padx=5, pady=5) 
+        self.input_city_address.grid(row=4, column=1, padx=5, pady=5) 
+        self.input_state_address.grid(row=4, column=2, padx=5, pady=5) 
+        self.input_country_address.grid(row=4, column=3, padx=5, pady=5) 
+        self.input_postal_code.grid(row=4, column=4, padx=5, pady=5)
 
     #Defining method for contact info's data
     def contact_info(self):
         #Contact Information Title
-        contact_info_title=tk.Label(self.personal_info_frame, text="Contact Information:", font=("Tahoma", 10, "bold"))
-        contact_info_title.grid(row=5, column=0, sticky="w")
+        self.contact_info_title=tk.Label(self.personal_info_frame, text="Contact Information:", font=("Tahoma", 10, "bold"))
+        self.contact_info_title.grid(row=5, column=0, sticky="w")
         
         #Contact Information Labels
-        label_phone_number = tk.Label(self.personal_info_frame, text="Phone Number")
-        label_phone_number.grid(row=6, column=0)
-        label_email = tk.Label(self.personal_info_frame, text="Email")
-        label_email.grid(row=6, column=1)
-        label_contact_person = tk.Label(self.personal_info_frame, text="Contact Person:")
-        label_contact_person.grid(row=6, column=2)
-        label_contperson_num = tk.Label(self.personal_info_frame, text="Phone Number:")
-        label_contperson_num.grid(row=7, column=2)
-        label_contperson_email = tk.Label(self.personal_info_frame, text="Email:")
-        label_contperson_email.grid(row=8, column=2)
-        label_contperson_rel = tk.Label(self.personal_info_frame, text="Relationship w/ Contact Person")
-        label_contperson_rel.grid(row=8, column=0)
+        self.label_phone_number = tk.Label(self.personal_info_frame, text="Phone Number")
+        self.label_phone_number.grid(row=6, column=0)
+        self.label_email = tk.Label(self.personal_info_frame, text="Email")
+        self.label_email.grid(row=6, column=1)
+        self.label_contact_person = tk.Label(self.personal_info_frame, text="Contact Person:")
+        self.label_contact_person.grid(row=6, column=2)
+        self.label_contperson_num = tk.Label(self.personal_info_frame, text="Phone Number:")
+        self.label_contperson_num.grid(row=7, column=2)
+        self.label_contperson_email = tk.Label(self.personal_info_frame, text="Email:")
+        self.label_contperson_email.grid(row=8, column=2)
+        self.label_contperson_rel = tk.Label(self.personal_info_frame, text="Relationship w/ Contact Person")
+        self.label_contperson_rel.grid(row=8, column=0)
         
         #Contact Information entry fields
-        input_phone_number = tk.Entry(self.personal_info_frame)
-        input_email = tk.Entry(self.personal_info_frame)
-        input_contact_person = tk.Entry(self.personal_info_frame, width = 30)
-        input_contperson_num = tk.Entry(self.personal_info_frame)
-        input_contperson_rel = tk.Entry(self.personal_info_frame)
-        input_contperson_email = tk.Entry(self.personal_info_frame)
-        input_phone_number.grid(row=7, column=0, padx=5, pady=5)
-        input_email.grid(row=7, column=1, padx=5, pady=5)
-        input_contact_person.grid(row=6, column=3, padx=5, pady=5)
-        input_contperson_num.grid(row=7, column=3, padx=5, pady=5, sticky="w")
-        input_contperson_rel.grid(row=8, column=1, padx=5, pady=5, sticky="w")
-        input_contperson_email.grid(row=8, column=3, padx=5, pady=5, sticky="w")
+        self.input_phone_number = tk.Entry(self.personal_info_frame)
+        self.input_email = tk.Entry(self.personal_info_frame)
+        self.input_contact_person = tk.Entry(self.personal_info_frame, width = 30)
+        self.input_contperson_num = tk.Entry(self.personal_info_frame)
+        self.input_contperson_rel = tk.Entry(self.personal_info_frame)
+        self.input_contperson_email = tk.Entry(self.personal_info_frame, width = 30)
+        self.input_phone_number.grid(row=7, column=0, padx=5, pady=5)
+        self.input_email.grid(row=7, column=1, padx=5, pady=5)
+        self.input_contact_person.grid(row=6, column=3, padx=5, pady=5)
+        self.input_contperson_num.grid(row=7, column=3, padx=5, pady=5, sticky="w")
+        self.input_contperson_rel.grid(row=8, column=1, padx=5, pady=5, sticky="w")
+        self.input_contperson_email.grid(row=8, column=3, padx=5, pady=5, sticky="w")
 
     def personal_info_configure(self):
         for widget in self.personal_info_frame.winfo_children():
@@ -136,8 +136,8 @@ class UserInterface:
 
     #First Question (Vaccination Status)
     def vaccine_status(self):
-        label_vaccine = tk.Label(self.health_info_frame, text ="Have you been vaccinated for COVID-19?") #Question label
-        label_vaccine.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = "w") #Position
+        self.label_vaccine = tk.Label(self.health_info_frame, text ="Have you been vaccinated for COVID-19?") #Question label
+        self.label_vaccine.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = "w") #Position
         var = tk.StringVar()    #Initializing Variable    
         
         #Initializing options in a list
@@ -145,29 +145,29 @@ class UserInterface:
         
         #Setting radiobuttons for the question
         for i in range (len(vac_status)):
-            input_vaccine = tk.Radiobutton(self.health_info_frame, text=vac_status[i], variable = var, value = vac_status[i]) 
-            input_vaccine.grid(row = 1+i, column = 0, padx = 0, pady = 0, sticky = "w")	     
+            self.input_vaccine = tk.Radiobutton(self.health_info_frame, text=vac_status[i], variable = var, value = vac_status[i]) 
+            self.input_vaccine.grid(row = 1+i, column = 0, padx = 0, pady = 0, sticky = "w")	     
    
     #Second Question (Symptoms)
     def symptoms (self):
-        label_symptoms = tk.Label(self.health_info_frame, text="Are you experiencing any symptoms in the past 7 days such as:")
-        label_symptoms.grid(row=14, column=0, padx=5, pady=5, sticky="w")
+        self.label_symptoms = tk.Label(self.health_info_frame, text="Are you experiencing any symptoms in the past 7 days such as:")
+        self.label_symptoms.grid(row=14, column=0, padx=5, pady=5, sticky="w")
                 
         #Initializing Symptoms option
-        check_symptom = []
-        options = ["Fever", "Cough", "Colds", "Sore Throat", "Headache", "Diarrhea", "Loss of taste or Smell", "None"]
+        self.check_symptom = []
+        self.options = ["Fever", "Cough", "Colds", "Sore Throat", "Headache", "Diarrhea", "Loss of taste or Smell", "None"]
                 
         #Checking symptoms data 
-        for i, option in enumerate(options):
+        for i, option in enumerate(self.options):
             var = tk.StringVar()
             input_symptom = tk.Checkbutton(self.health_info_frame, text=option, variable=var, onvalue = option, offvalue="", anchor = "w")
             input_symptom.grid(row=15 + (i // 2), column=i % 2, padx=0, pady=0, sticky="w")
-            check_symptom.append(var)
+            self.check_symptom.append(var)
     
     #Third Question (Exposure to a probable positive case)
     def exposure(self):
-        label_exposure = tk.Label(self.health_info_frame, text ="Have you had exposure to a probable or confirmed case in the last 14 days?")#Question label
-        label_exposure.grid(row=21, column=0, padx=5, pady= 5, sticky="w") #Position
+        self.label_exposure = tk.Label(self.health_info_frame, text ="Have you had exposure to a probable or confirmed case in the last 14 days?")#Question label
+        self.label_exposure.grid(row=21, column=0, padx=5, pady= 5, sticky="w") #Position
         ex_var = tk.StringVar()    #Initializing Variable    
         
         #Initializing options in a list
@@ -175,16 +175,16 @@ class UserInterface:
         
         #Setting radiobuttons for the question
         for i in range (len(ex_status)):
-            input_exposure = tk.Radiobutton(self.health_info_frame, text=ex_status[i], variable = ex_var, value = ex_status[i]) 
-            input_exposure.grid(row = 22+i, column = 0, padx = 0, pady = 0, sticky = "w")
+            self.input_exposure = tk.Radiobutton(self.health_info_frame, text=ex_status[i], variable = ex_var, value = ex_status[i]) 
+            self.input_exposure.grid(row = 22+i, column = 0, padx = 0, pady = 0, sticky = "w")
 
     #Fourth Question (Contact with someone with symptoms)
     def contact (self):
-        label_contact = tk.Label(self.health_info_frame, text="Have you had in contact with somebody with body pains\n"
+        self.label_contact = tk.Label(self.health_info_frame, text="Have you had in contact with somebody with body pains\n"
                                                 "headache, sore throat, fever, diarrhea, caugh, colds, shortness of breath,\n"
                                                 "loss of taste or loss of smell in the past 7 days?",
                                 justify="left")  #Question label
-        label_contact.grid(row=24, column=0, padx=5, pady=5, sticky ="w") #Position
+        self.label_contact.grid(row=24, column=0, padx=5, pady=5, sticky ="w") #Position
         contact_var = tk.StringVar() #Initializing Variable
         
         #Initializing options in a list
@@ -197,8 +197,8 @@ class UserInterface:
     
     #Fifth Question (If tested for COVID)
     def test (self):
-        label_test = tk.Label(self.health_info_frame, text="Have you been tested for COVID-19 in the last 14 days?") #Question label
-        label_test.grid(row = 28, column =0, padx = 5, pady =5, sticky ="w") #Position
+        self.label_test = tk.Label(self.health_info_frame, text="Have you been tested for COVID-19 in the last 14 days?") #Question label
+        self.label_test.grid(row = 28, column =0, padx = 5, pady =5, sticky ="w") #Position
         test_var = tk.StringVar() #Initializing Variable
         
         #Initializing options in a list
