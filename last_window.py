a7d0f838611e02f55cb744770f0ab71d7c588285
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-25 15:33:17 +0800

    last window: Buttons added

--- last_window.py
+++ last_window.py
@@ -23,7 +23,13 @@ class LastWindow:
 
         # Bind the window resize event 
         self.last.bind("<Configure>", self.resize_image)
-        
+
+        #Call the buttons
+        self.covid_info_button()
+        self.app_info_button()
+        self.home_button()
+        self.exit_button()
+
         #Run Mainloop
         self.last.mainloop()
     #Updating background image
@@ -44,6 +50,74 @@ class LastWindow:
         self.update_background_image()
 
     #Add buttons
+    #Covid Info button
+    def covid_info_button(self):
+            # Load the image for the button
+            image = Image.open("covid_info.png")
+
+            # Resize the image 
+            new_width = 150
+            new_height = 150
+            resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+            # Create the PhotoImage from the resized image
+            button_image = ImageTk.PhotoImage(resized_image)
+
+            # Create the button with the resized image
+            button = tk.Button(self.last, image=button_image, borderwidth=0)
+            button.image = button_image 
+            button.place(x=55, y=50)
+    #Button for app info
+    def app_info_button(self):
+        # Load the image for the button
+        image = Image.open("app_info.png")
+
+        # Resize the image 
+        new_width = 150
+        new_height = 150
+        resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+        # Create the PhotoImage from the resized image
+        button_image = ImageTk.PhotoImage(resized_image)
+
+        # Create the button with the resized image
+        button = tk.Button(self.last, image=button_image, borderwidth=0)
+        button.image = button_image 
+        button.place(x=55, y=235) 
+    #Button for home
+    def home_button(self):
+        # Load the image for the button
+        image = Image.open("home_button.png")
+
+        # Resize the image 
+        new_width = 150
+        new_height = 150
+        resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+        # Create the PhotoImage from the resized image
+        button_image = ImageTk.PhotoImage(resized_image)
+
+        # Create the button with the resized image
+        button = tk.Button(self.last, image=button_image, borderwidth=0)
+        button.image = button_image 
+        button.place(x=55, y=420) 
+    #Button for exit
+    def exit_button(self):
+        # Load the image for the button
+        image = Image.open("exit_button.png")
+
+        # Resize the image 
+        new_width = 200
+        new_height = 70
+        resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+        # Create the PhotoImage from the resized image
+        button_image = ImageTk.PhotoImage(resized_image)
+
+        # Create the button with the resized image
+        button = tk.Button(self.last, image=button_image, borderwidth=0)
+        button.image = button_image 
+        button.place(x=30, y=580) 
     #For button commands
 
 # Create an instance of the last window class(test)
